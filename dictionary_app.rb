4b082681dda46942ce3e2f446d7637c421d5c799
bartstachurski <bart.stachurski@gmail.com>
2019-06-20 21:37:38 -0400

    add ability to look up top example and pronunciation of input word - start ability to open audio file from wordnik but not complete

--- dictionary_app.rb
+++ dictionary_app.rb
@@ -1,8 +1,25 @@
 require 'http'
 
-p "Please enter a word you would like the definition for:"
-user_input = gets.chomp 
+user_input = nil
 
-dictionary = HTTP.get("https://api.wordnik.com/v4/word.json/#{user_input}/definitions?limit=200&includeRelated=false&useCanonical=false&includeTags=false&api_key=")
+until user_input == "q"
 
-p dictionary.parse[1]["text"]
+  p "Please enter a word you would like the definition, top example, and pronunciation of. You can keep entering words after your first word. Type q to quit"
+  user_input = gets.chomp 
+
+  break if user_input == "q"
+
+  dictionary = HTTP.get("https://api.wordnik.com/v4/word.json/#{user_input}/definitions?limit=200&includeRelated=false&useCanonical=false&includeTags=false&api_key=ac6099e63826b8650f05e22c4cc08baa2f21668e3f16176fd")
+
+  top_example = HTTP.get("https://api.wordnik.com/v4/word.json/#{user_input}/topExample?useCanonical=false&api_key=ac6099e63826b8650f05e22c4cc08baa2f21668e3f16176fd")
+
+  pronunciation = HTTP.get("https://api.wordnik.com/v4/word.json/#{user_input}/pronunciations?useCanonical=false&limit=50&api_key=ac6099e63826b8650f05e22c4cc08baa2f21668e3f16176fd")
+
+  audio = HTTP.get("https://api.wordnik.com/v4/word.json/#{user_input}/audio?useCanonical=false&limit=50&api_key=ac6099e63826b8650f05e22c4cc08baa2f21668e3f16176fd")
+
+  p "The defintion of #{user_input} is #{dictionary.parse[1]["text"]}"
+  p "The top example of #{user_input} is #{top_example.parse["text"]}"
+  p "The pronunciation of #{user_input} is #{pronunciation.parse[0]["raw"]}"
+  p audio.parse
+
+end
